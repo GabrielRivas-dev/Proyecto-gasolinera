72b1dfa1bdcee819f3b22c3fd44045d38fed8cee
GabrielRivas-dev <gaalex300@gmail.com>
2025-01-29 21:21:41 -0400

    Add files via upload
    Gas Station

---
+++ GasStation/src/main/java/com/gasolinera/demo/GasolineraMsApplication.java
@@ -0,0 +1,13 @@
+package com.gasolinera.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolineraMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasolineraMsApplication.class, args);
+	}
+
+}
---
+++ GasStation/src/test/java/com/gasolinera/demo/GasolineraMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasolinera.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasolineraMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
